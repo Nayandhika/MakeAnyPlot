e21482eabf30ce776cad7d672846c601170922a7
Nayandhika Rebeca Paul <140233239+Nayandhika@users.noreply.github.com>
2024-07-06 16:24:02 +0530

    Update script.py

--- script.py
+++ script.py
@@ -16,25 +16,14 @@ st.set_page_config(
 # textColor = st.get_option("theme.textColor")
 # font=st.get_option("theme.font")
 
-main_bg = "sample1.jpg"
-main_bg_ext = "jpg"
-
-side_bg = "sample2.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
-    <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
-    </style>
-    """,
-    unsafe_allow_html=True
-)
+css="""
+<style>
+    [data-testid="stForm"] {
+        background: LightBlue;
+    }
+</style>
+"""
+st.write(css, unsafe_allow_html=True)
 
 st.title("MakeAnyPlot")
 st.markdown("""---""")
