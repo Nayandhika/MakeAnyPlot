5d56c64e9867d016044f32d8b6c3546736ad878f
Nayandhika Rebeca Paul <140233239+Nayandhika@users.noreply.github.com>
2024-07-06 16:46:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -17,25 +17,27 @@ st.set_page_config(
 # textColor = st.get_option("theme.textColor")
 # font=st.get_option("theme.font")
 
-main_bg = "image1.jpg"
-main_bg_ext = "jpg"
+@st.cache(allow_output_mutation=True)
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
 
-side_bg = "image1.jpg"
-side_bg_ext = "jpg"
-
-st.markdown(
-    f"""
+def set_png_as_page_bg(png_file):
+    bin_str = get_base64_of_bin_file(png_file)
+    page_bg_img = '''
     <style>
-    .reportview-container {{
-        background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
-    }}
-   .sidebar .sidebar-content {{
-        background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
-    }}
+    body {
+    background-image: url("data:image1/jpg;base64,%s");
+    background-size: cover;
+    }
     </style>
-    """,
-    unsafe_allow_html=True
-)
+    ''' % bin_str
+    
+    st.markdown(page_bg_img, unsafe_allow_html=True)
+    return
+
+set_png_as_page_bg('background.png')
 
 st.title("MakeAnyPlot")
 st.markdown("""---""")
