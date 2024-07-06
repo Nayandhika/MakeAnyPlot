304e25234189125e6ce145d8fc64aa2502a06d32
Nayandhika Rebeca Paul <140233239+Nayandhika@users.noreply.github.com>
2024-07-06 16:21:32 +0530

    Update script.py

--- script.py
+++ script.py
@@ -25,10 +25,10 @@ side_bg_ext = "jpg"
 st.markdown(
     f"""
     <style>
-    .reportview-container {
+    .reportview-container {{
         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()})
     }}
-   .sidebar .sidebar-content {
+   .sidebar .sidebar-content {{
         background: url(data:image/{side_bg_ext};base64,{base64.b64encode(open(side_bg, "rb").read()).decode()})
     }}
     </style>
