3b583d70744d5f9f056b8a77823b6210b2c4f85c
Nayandhika Rebeca Paul <140233239+Nayandhika@users.noreply.github.com>
2024-07-06 11:35:45 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 import numpy as np
 st.set_page_config(
-    page_title="MakeAnyPlot 📈",
+    page_title="MakeAnyPlot",
     page_icon="📈",  
     layout="wide"
 )
